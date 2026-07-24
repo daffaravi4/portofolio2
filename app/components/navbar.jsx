1a34d0b88f9133402fdda06600bf09a541c448e2
daffaravi4 <daffaravi4@gmail.com>
2026-07-24 07:43:19 +0700

    fix lanyard

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
 
       {/* Hamburger - HP saja */}
       <button
-        className="md:hidden text-white text-3xl -translate-x-[120px]"
+        className="md:hidden text-white text-3xl -translate-x-[100px]"
         onClick={() => setOpen(!open)}
       >
         {open ? "✕" : "☰"}
